f4436f79a92453796900868e72f1a3dd3613b199
Unity_one_love <sazanovichmaksim@gmail.com>
2025-01-30 19:09:48 +0300

    Lab_13

---
+++ C++/1 course/2 sem/Lab_13/Lab_13/Lab_13.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct element
+{
+	int inf;
+	element* next_adress;
+};
+
+struct stack
+{
+	element* top = nullptr;
+	int size = 0;
+
+	bool is_empty()
+	{
+		if (top)
+			return false;
+		else
+			return true;
+	}
+
+	void push(int inf)
+	{
+		element* el = new element;
+		el->inf = inf;
+		el->next_adress = top;
+		top = el;
+		size++;
+	}
+
+	void pop()
+	{
+		element* el = top;
+		top = top->next_adress;
+		delete el;
+		size--;
+	}
+
+	void print()
+	{
+		element* el = top;
+		while (el != nullptr)
+		{
+			cout << el->inf << " ";
+			el = el->next_adress;
+		}
+		cout << endl;
+	}
+
+	element* search(int x)
+	{
+		if (!top)
+			return nullptr;
+		element* el = top;
+		while (el->inf != x && el->next_adress != nullptr)
+			el = el->next_adress;
+		if (el->inf == x)
+			return el;
+		else
+			return nullptr;
+	}
+
+	element* search_before(int x)
+	{
+		if (!top || !top->next_adress)
+			return nullptr;
+		element* el = top;
+		while (el->next_adress->inf != x && el->next_adress->next_adress != nullptr)
+			el = el->next_adress;
+		if (el->next_adress->inf == x)
+			return el;
+		else
+			return nullptr;
+	}
+
+	void del(int x)
+	{
+		if (!top)
+			return;
+		if (top->inf == x)
+			pop();
+		element* el, * pr_el;
+		pr_el = search_before(x);
+		el = pr_el->next_adress;
+		pr_el->next_adress = pr_el->next_adress->next_adress;
+		delete el;
+	}
+
+	// Обмен двух следующих за указанным элементов
+	void exchange(element* el)
+	{
+		element* el_2;
+		el_2 = el->next_adress->next_adress;
+		el->next_adress->next_adress = el_2->next_adress;
+		el_2->next_adress = el->next_adress;
+		el->next_adress = el_2;
+	}
+
+	void print_element(element* el)
+	{
+		cout << el->inf << endl;
+	}
+};
+
+int main()
+{
+	stack s;
+	s.push(10);
+	s.push(9);
+	s.push(8);
+	s.push(7);
+	s.push(6);
+	s.push(5);
+	s.push(4);
+	s.push(3);
+	s.push(2);
+	s.push(1);
+	s.print();
+	element* cur = s.top;
+	int x;
+	while (cur != nullptr && cur->next_adress != nullptr && cur->next_adress->next_adress != nullptr)
+	{
+		x = cur->next_adress->next_adress->inf;
+		s.del(x);
+		cur = cur->next_adress->next_adress;
+	}
+	s.print();
+
+	return 0;
+}
+
