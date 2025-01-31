f4d17d0770b0b638d2abfb7a376a79b35ae9fff9
Unity_one_love <sazanovichmaksim@gmail.com>
2025-01-31 18:25:35 +0300

    Lab_13 Input

--- C++/1 course/2 sem/Lab_13/Lab_13/Lab_13.cpp
+++ C++/1 course/2 sem/Lab_13/Lab_13/Lab_13.cpp
@@ -107,16 +107,26 @@ struct stack
 int main()
 {
 	stack s;
-	s.push(10);
-	s.push(9);
-	s.push(8);
-	s.push(7);
-	s.push(6);
-	s.push(5);
-	s.push(4);
-	s.push(3);
-	s.push(2);
-	s.push(1);
+	int n;
+	int e;
+	cout << "Enter count of element ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Enter element ";
+		cin >> e;
+		s.push(e);
+	}
+	//s.push(10);
+	//s.push(9);
+	//s.push(8);
+	//s.push(7);
+	//s.push(6);
+	//s.push(5);
+	//s.push(4);
+	//s.push(3);
+	//s.push(2);
+	//s.push(1);
 	s.print();
 	element* cur = s.top;
 	int x;
